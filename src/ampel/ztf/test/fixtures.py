d301184c4e78f7c69f14fdcca11cf3925dc9c08f
Jakob van Santen <jvansanten@gmail.com>
2020-01-08 12:43:35 +0100

    Add ZUDS archive clients

--- src/ampel/ztf/test/fixtures.py
+++ src/ampel/ztf/test/fixtures.py
@@ -6,6 +6,30 @@ import pytest
 def alert_tarball():
 	return join(dirname(__file__), 'test-data', 'ztf_public_20180819_mod1000.tar.gz')
 
+@pytest.fixture(scope='session', params=['ztf_20200106_programid2_zuds.tar.gz', 'ztf_20200106_programid2_zuds_stack.tar.gz'])
+def zuds_alert_generator(request):
+	import itertools
+	import fastavro
+	from ampel.pipeline.t0.load.TarballWalker import TarballWalker
+	def alerts(with_schema=False):
+		candids = set()
+		try:
+			atat = TarballWalker(join(dirname(__file__), 'test-data', request.param))
+			for fileobj in itertools.islice(atat.get_files(), 0, 1000, 1):
+				reader = fastavro.reader(fileobj)
+				alert = next(reader)
+				if alert['candid'] in candids:
+					continue
+				else:
+					candids.add(alert['candid'])
+				if with_schema:
+					yield alert, reader.writer_schema
+				else:
+					yield alert
+		except FileNotFoundError:
+			raise pytest.skip("{} does not exist".format(request.param))
+	return alerts
+
 @pytest.fixture(scope='session')
 def alert_generator(alert_tarball):
 	import itertools
