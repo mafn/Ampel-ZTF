ab0111d3d769efe3c0c2d7a2c6715ee7f16d6d6e
vbrinnel <valery.brinnel@gmail.com>
2020-05-29 17:13:28 +0200

    New alg for generating ampel ids

--- ampel/ztf/utils.py
+++ ampel/ztf/utils.py
@@ -4,27 +4,19 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 07.06.2018
-# Last Modified Date: 19.03.2020
+# Last Modified Date: 27.04.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import overload, List, Union, Tuple
+from typing import overload, List, Union, Iterable
+from ampel.type import StrictIterable
 
-
-number_map = {
-	'10': 'a', '11': 'b', '12': 'c', '13': 'd', '14': 'e', '15': 'f',
-	'16': 'g', '17': 'h', '18': 'i', '19': 'j', '20': 'k', '21': 'l',
-	'22': 'm', '23': 'n', '24': 'o', '25': 'p', '26': 'q', '27': 'r',
-	'28': 's', '29': 't', '30': 'u', '31': 'v', '32': 'w', '33': 'x',
-	'34': 'y', '35': 'z'
-}
-
-letter_map = {
-	'a': '10', 'b': '11', 'c': '12', 'd': '13', 'e': '14', 'f': '15',
-	'g': '16', 'h': '17', 'i': '18', 'j': '19', 'k': '20', 'l': '21',
-	'm': '22', 'n': '23', 'o': '24', 'p': '25', 'q': '26', 'r': '27',
-	's': '28', 't': '29', 'u': '30', 'v': '31', 'w': '32', 'x': '33',
-	'y': '34', 'z': '35'
-}
+# Optimization variables
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+ab = {alphabet[i]: i for i in range(26)}
+rg = (6, 5, 4, 3, 2, 1, 0)
+powers = tuple(26**i for i in (6, 5, 4, 3, 2, 1, 0))
+enc_ztf_years = {str(i + 17): i for i in range(16)}
+dec_ztf_years = {i: str(i + 17) for i in range(16)}
 
 
 @overload
@@ -32,59 +24,114 @@ def to_ampel_id(ztf_id: str) -> int:
 	...
 
 @overload
-def to_ampel_id(ztf_id: Union[List[str], Tuple[str, ...]]) -> List[int]:
+def to_ampel_id(ztf_id: StrictIterable[str]) -> List[int]:
 	...
 
-def to_ampel_id(ztf_id: Union[str, List[str], Tuple[str, ...]]) -> Union[int, List[int]]:
+def to_ampel_id(ztf_id: Union[str, StrictIterable[str]]) -> Union[int, List[int]]:
 	"""
-	:returns: Ampel ID (int).
+	:returns: ampel id (positive integer).
+
+	====== First 4 bits encode the ZTF year (until max 2032) =====
+
+	In []: to_ampel_id('ZTF17aaaaaaa')
+	Out[]: 0
+
+	In []: to_ampel_id('ZTF18aaaaaaa')
+	Out[]: 1
+
+	In []: to_ampel_id('ZTF19aaaaaaa')
+	Out[]: 2
+
+	====== Bits onwards encode the ZTF name converted from base 26 into base 10 =====
+
+	In []: to_ampel_id('ZTF17aaaaaaa')
+	Out[]: 0
+
+	In []: to_ampel_id('ZTF17aaaaaab')
+	Out[]: 16
+
+	In []: to_ampel_id('ZTF17aaaaaac')
+	Out[]: 32
+
+	====== Biggest numerical value is < 2**37 =====
+
+	Out[]: In []: to_ampel_id('ZTF32zzzzzzz')
+	Out[]: 128508962815
+
+	========================================================================
+	This encoding allows to save most of ZTF transients (up until ~akzzzzzz)
+	with a signed int32. Note: MongoDB imposes signed integers and chooses
+	automatically the right _id type (int32/int64/...) per document
+	=====================================================================
+
+	In []: to_ampel_id('ZTF20akzzzzzz') < 2**31
+	Out[]: True
+
+	In []: to_ampel_id('ZTF20alzzzzzz') < 2**31
+	Out[]: False
+
+	=====================================================================
+
+	Note: slightly slower than the legacy method
+
+	-> Legacy
+	%timeit to_ampel_id("ZTF19abcdfef")
+	949 ns ± 9.08 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
+
+	-> This method
+	%timeit to_ampel_id('ZTF19abcdfef')
+	1.51 µs ± 35.5 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
+
+	but encodes ZTF ids with 37 bits instead of 52 bits
+
+	In []: 2**36 < to_ampel_id('ZTF32zzzzzzz') < 2**37
+	Out[]: True
+
+	from ampel.ztf.legacy_utils import to_ampel_id as to_legacy_ampel_id
+	In []:  2**51 < to_legacy_ampel_id("ZTF32zzzzzz") < 2**52
+	Out []: True
 	"""
 
-	# Handle sequences
-	if isinstance(ztf_id, (list, tuple)):
-		return [to_ampel_id(name) for name in ztf_id]
+	if isinstance(ztf_id, str):
+		num = 0
+		s2 = ztf_id[5:]
+		for i in rg:
+			num += ab[s2[i]] * powers[i]
+		return (num << 4) + enc_ztf_years[ztf_id[3:5]]
 
-	return int(
-		"".join(
-			(
-				ztf_id[3:5],
-				letter_map[ztf_id[5]],
-				letter_map[ztf_id[6]],
-				letter_map[ztf_id[7]],
-				letter_map[ztf_id[8]],
-				letter_map[ztf_id[9]],
-				letter_map[ztf_id[10]],
-				letter_map[ztf_id[11]]
-			)
-		)
-	)
+	return [to_ampel_id(name) for name in ztf_id]
+
 
 @overload
 def to_ztf_id(ampel_id: int) -> str:
 	...
 
 @overload
-def to_ztf_id(ampel_id: Union[List[int], Tuple[int, ...]]) -> List[str]:
+def to_ztf_id(ampel_id: Iterable[int]) -> List[str]:
 	...
 
-def to_ztf_id(ampel_id: Union[int, List[int], Tuple[int, ...]]) -> Union[str, List[str]]:
+def to_ztf_id(ampel_id: Union[int, Iterable[int]]) -> Union[str, List[str]]:
 	"""
-	:param ampel_id: int or list of int
-	:returns: ZTF ID (string).
+	%timeit to_ztf_id(274878346346)
+	1.54 µs ± 77.9 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
 	"""
 	# Handle sequences
-	if isinstance(ampel_id, (list, tuple)):
-		return [to_ztf_id(l) for l in ampel_id]
+	if isinstance(ampel_id, int):
 
-	str_long = str(ampel_id)
+		# int('00001111', 2) bitmask equals 15
+		year = dec_ztf_years[ampel_id & 15]
 
-	return "ZTF%s%s%s%s%s%s%s%s" % (
-		str_long[0:2],
-		number_map[str_long[2:4]],
-		number_map[str_long[4:6]],
-		number_map[str_long[6:8]],
-		number_map[str_long[8:10]],
-		number_map[str_long[10:12]],
-		number_map[str_long[12:14]],
-		number_map[str_long[14:16]]
-	)
+		# Shift base10 encoded value 4 bits to the right
+		ampel_id = ampel_id >> 4
+
+		# Convert back to base26
+		l = ['a', 'a', 'a', 'a', 'a', 'a', 'a']
+		for i in rg:
+			l[i] = alphabet[ampel_id % 26]
+			ampel_id //= 26
+			if not ampel_id:
+				break
+
+		return f"ZTF{year}{''.join(l)}"
+
+	return [to_ztf_id(l) for l in ampel_id]
