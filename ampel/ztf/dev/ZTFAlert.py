9ee8ed4dba126772539b7fbba1840bd9411f81b5
vbrinnel <valery.brinnel@gmail.com>
2020-07-31 10:54:13 +0200

    ZTFAlert: incomplete v0.7 compatibility update

--- ampel/ztf/dev/ZTFAlert.py
+++ ampel/ztf/dev/ZTFAlert.py
@@ -1,75 +1,54 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-ZTF/src/ampel/ztf/utils/ZIAlert.py
+# File              : Ampel-ZTF/ampel/ztf/dev/ZTFAlert.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 24.06.2018
-# Last Modified Date: 09.01.2020
+# Last Modified Date: 31.07.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 import fastavro, os, time
-from typing import Dict, Optional, Any, List
+from typing import Dict, Optional, List
 from ampel.view.LightCurve import LightCurve
 from ampel.view.TransientView import TransientView
 from ampel.content.DataPoint import DataPoint
 from ampel.alert.PhotoAlert import PhotoAlert
-from ampel.ztf.t0.ingest.ZIPhotoDataShaper import ZIPhotoDataShaper
-from ampel.ztf.t0.load.ZIAlertShaper import ZIAlertShaper
+from ampel.ztf.alert.ZiAlertSupplier import ZiAlertSupplier
 
 
 class ZTFAlert:
 
 
 	@classmethod
-	def _first_shape(cls,
-		file_path: Optional[str] = None,
-		content: Optional[Dict] = None
-	) -> Dict[str, Any]:
-		""" AKA filter shape """
-
+	def to_photo_alert(cls, file_path: Optional[str] = None) -> PhotoAlert:
+		"""
+		Creates and returns an instance of ampel.view.LightCurve using a ZTF IPAC alert.
+		"""
+		als = ZiAlertSupplier(deserialize="avro")
 		if file_path:
-			content = cls._load_alert(file_path)
+			from ampel.alert.load.FileAlertLoader import FileAlertLoader
+			als.set_alert_source(FileAlertLoader(file_path))
 
-		return ZIAlertShaper.shape(content)
-
-
-
-	@classmethod
-	def to_photo_alert(cls,
-		file_path: Optional[str] = None,
-		content: Optional[Dict] = None
-	) -> PhotoAlert:
-		"""
-		Creates and returns an instance of ampel.view.LightCurve using a ZTF IPAC alert.
-		"""
-		shaped_content = cls._first_shape(file_path, content)
-
-		return PhotoAlert(
-			shaped_content['stockId'],
-			shaped_content['ro_pps'],
-			shaped_content['ro_uls']
-		)
+		return next(als)
 
 
 	@classmethod
-	def to_lightcurve(cls,
-		file_path: Optional[str] = None,
-		content: Optional[Dict] = None
-	) -> LightCurve:
+	def to_lightcurve(cls, file_path: Optional[str] = None) -> LightCurve:
 		"""
 		Creates and returns an instance of ampel.view.LightCurve using a ZTF IPAC alert.
 		"""
 
-		shaped_content = cls._first_shape(file_path, content)
+		pal = cls.to_photo_alert(file_path)
 		photo_shaper = ZIPhotoDataShaper()
 
 		# Build upper limit ids (done by ingester for now)
-		for el in shaped_content['uls']:
-			el['_id'] = int("%i%s%i" % (
-				(2457754.5 - el['jd']) * 1000000,
-				str(el['pid'])[8:10],
-				round(abs(el['diffmaglim']) * 1000)
-			))
+		if pal.uls:
+			for el in pal.uls:
+				el['_id'] = int("%i%s%i" % (
+					(2457754.5 - el['jd']) * 1000000,
+					str(el['pid'])[8:10],
+					round(abs(el['diffmaglim']) * 1000)
+				))
 
 		uls = photo_shaper.ampelize(
 			shaped_content['uls'],
