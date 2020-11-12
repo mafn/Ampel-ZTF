bfe261776787a1a6f4dedc4c3739677bbf576cd8
Jakob van Santen <jvansanten@gmail.com>
2020-11-12 11:30:00 +0100

    FritzReport: refactor as async

--- ampel/ztf/t3/complement/FritzReport.py
+++ ampel/ztf/t3/complement/FritzReport.py
@@ -7,15 +7,18 @@
 # Date              : 03.11.2020
 # Last Modified By  : Jakob van Santen <jakob.van.santen@desy.de>
 
-
-from functools import cached_property
+import asyncio
 from typing import Any, Dict, Iterable, Optional, Tuple
+
+import nest_asyncio
 
 from ampel.core.AmpelBuffer import AmpelBuffer
 from ampel.model.Secret import Secret
 from ampel.t3.complement.AbsT3DataAppender import AbsT3DataAppender
-
-from ampel.ztf.t3.skyportal.SkyPortalClient import SkyPortalClient, SkyPortalAPIError
+from ampel.ztf.t3.skyportal.SkyPortalClient import (
+    SkyPortalAPIError,
+    SkyPortalClient,
+)
 
 
 class FritzReport(SkyPortalClient, AbsT3DataAppender):
@@ -28,12 +31,14 @@ class FritzReport(SkyPortalClient, AbsT3DataAppender):
     #: API token
     token: Secret[str] = {"key": "fritz/jno/ampelbot"}  # type: ignore[assignment]
 
-    def get_catalog_item(self, names: Tuple[str, ...]) -> Optional[Dict[str, Any]]:
+    async def get_catalog_item(
+        self, names: Tuple[str, ...]
+    ) -> Optional[Dict[str, Any]]:
         """Get catalog entry associated with the stock name"""
         for name in names:
             if name.startswith("ZTF"):
                 try:
-                    record = self.get(f"sources/{name}")
+                    record = await self.get(f"sources/{name}")
                 except SkyPortalAPIError:
                     return None
                 # strip out Fritz chatter
@@ -44,14 +49,24 @@ class FritzReport(SkyPortalClient, AbsT3DataAppender):
                 }
         return None
 
+    async def update_record(self, record: AmpelBuffer) -> None:
+        if (stock := record["stock"]) is None:
+            raise ValueError(f"{type(self).__name__} requires stock records")
+        item = await self.get_catalog_item(
+            tuple(name for name in (stock["name"] or []) if isinstance(name, str))
+        )
+        if record.get("extra") is None or record["extra"] is None:
+            record["extra"] = {self.__class__.__name__: item}
+        else:
+            record["extra"][self.__class__.__name__] = item
+
     def complement(self, records: Iterable[AmpelBuffer]) -> None:
-        for record in records:
-            if (stock := record["stock"]) is None:
-                raise ValueError(f"{type(self).__name__} requires stock records")
-            item = self.get_catalog_item(
-                tuple(name for name in (stock["name"] or []) if isinstance(name, str))
-            )
-            if record.get("extra") is None or record["extra"] is None:
-                record["extra"] = {self.__class__.__name__: item}
-            else:
-                record["extra"][self.__class__.__name__] = item
+        # Patch event loop to be reentrant if it is already running, e.g.
+        # within a notebook
+        try:
+            if asyncio.get_event_loop().is_running():
+                nest_asyncio.apply()
+        except RuntimeError:
+            # second call raises: RuntimeError: There is no current event loop in thread 'MainThread'.
+            ...
+        asyncio.run(asyncio.gather(*[self.update_record(record) for record in records]))
