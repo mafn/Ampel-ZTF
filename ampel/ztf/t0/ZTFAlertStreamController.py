afc6d2aafe5555aee64647a893fedde9f87ad0d6
Jakob van Santen <jakob.van.santen@desy.de>
2021-03-25 14:49:03 +0100

    ZTFAlertStreamController: refactor alert suppliers

--- ampel/ztf/t0/ZTFAlertStreamController.py
+++ ampel/ztf/t0/ZTFAlertStreamController.py
@@ -10,129 +10,27 @@
 import asyncio
 import copy
 import logging
-from os.path import basename
-from typing import Any, Dict, List, Literal, Optional, Union, Set, Sequence
+from collections import Counter
+from typing import Any, Dict, List, Optional, Set, Sequence
 
 import backoff
 import requests
 
 from ampel.abstract.AbsProcessController import AbsProcessController
 from ampel.abstract.AbsSecretProvider import AbsSecretProvider
-from ampel.alert.load.TarAlertLoader import TarAlertLoader
 from ampel.config.AmpelConfig import AmpelConfig
 from ampel.core.AmpelContext import AmpelContext
 from ampel.model.ProcessModel import ProcessModel
-from ampel.model.Secret import Secret
-from ampel.model.StrictModel import StrictModel
 from ampel.model.UnitModel import UnitModel
-from requests.sessions import Session
 from ampel.util import concurrent
-from ampel.ztf.alert.ZiAlertSupplier import ZiAlertSupplier
-from ampel.ztf.t0.load.UWAlertLoader import UWAlertLoader
 
 
 log = logging.getLogger(__name__)
 
 
-class ArchiveSink(StrictModel):
-    db: str
-    auth: Secret[dict] = {"key": "ztf/archive/writer"} # type: ignore[assignment]
-
-
-class KafkaSource(StrictModel):
-    broker: str
-    group: str
-    stream: Literal["ztf_uw_private", "ztf_uw_public"]
-    timeout: int = 3600
-    archive: Optional[ArchiveSink]
-
-    def label(self):
-        return f"kafka.{self.group}"
-
-    def get(self) -> ZiAlertSupplier:
-        from ampel.ztf.t0.ArchiveUpdater import ArchiveUpdater # type: ignore[import]
-
-        supplier = ZiAlertSupplier()
-        supplier.set_alert_source(
-            UWAlertLoader(
-                stream=self.stream,
-                bootstrap=self.broker,
-                group_name=self.group,
-                archive_updater=(
-                    ArchiveUpdater(self.archive.db, connect_args=self.archive.auth.get())
-                    if self.archive else None
-                ),
-                timeout=self.timeout,
-                ).alerts()
-        )
-        return supplier
-
-
-class TarballSource(StrictModel):
-    filename: str
-    serialization: Optional[Literal["avro", "json"]] = "avro"
-
-    def label(self):
-        return basename(self.filename)
-
-    def get(self) -> ZiAlertSupplier:
-        supplier = ZiAlertSupplier(deserialize=self.serialization)
-        supplier.set_alert_source(TarAlertLoader(file_path=self.filename))
-        return supplier
-
-
-class ArchiveSource(StrictModel):
-    #: Base URL of archive service
-    archive: str = "https://ampel.zeuthen.desy.de/api/ztf/archive"
-    #: A stream identifier, created via POST /api/ztf/archive/streams/
-    stream: str
-
-    def label(self):
-        return f"archive.{self.stream}"
-    
-    def get_alerts(self):
-        with requests.Session() as session:
-            while True:
-                chunk = self._get_chunk(session)
-                try:
-                    yield from chunk["alerts"]
-                except GeneratorExit:
-                    log.error(
-                        f"Chunk from stream {self.stream} partially consumed. "
-                        f"Ensure that iter_max is a multiple of the chunk size."
-                    )
-                    raise
-                if len(chunk["alerts"]) == 0 and chunk["chunks_remaining"] == 0:
-                    break
-
-    @backoff.on_exception(
-        backoff.expo,
-        requests.HTTPError,
-        giveup=lambda e: e.response.status_code not in {503, 429},
-        max_time=600,
-    )
-    def _get_chunk(self, session: requests.Session) -> Dict[str, Any]:
-        response = session.get(f"{self.archive}/stream/{self.stream}/chunk")
-        response.raise_for_status()
-        return response.json()
-
-    def get(self) -> ZiAlertSupplier:
-        supplier = ZiAlertSupplier(deserialize=None)
-        supplier.set_alert_source(self.get_alerts())
-        return supplier
-
-
-class AlertSource(StrictModel):
-    source: Union[KafkaSource, TarballSource, ArchiveSource]
-
-    def get(self) -> ZiAlertSupplier:
-        return self.source.get()
-
-
 class ZTFAlertStreamController(AbsProcessController):
 
     priority: str
-    source: Union[KafkaSource, TarballSource, ArchiveSource]
     multiplier: int = 1
 
     def __init__(self, **kwargs) -> None:
@@ -150,7 +48,6 @@ class ZTFAlertStreamController(AbsProcessController):
         self.processes = processes
         self.secrets = secrets
         self._process = self.merge_processes(list(self.processes))
-        self._process.name = self.source.label()
 
     @staticmethod
     def merge_processes(processes: List[ProcessModel]) -> ProcessModel:
@@ -178,6 +75,8 @@ class ZTFAlertStreamController(AbsProcessController):
             assert strip(process.processor.config) == strip(pm.processor.config), "AlertProcessor configs are compatible"
             assert strip(process.processor.override) == strip(pm.processor.override), "AlertProcessor overrides are compatible"
             process.processor.config["directives"] += pm.processor.config["directives"]
+        
+        process.name = Counter([proc.name.split('|')[-1] for proc in processes]).most_common(1)[0][0]
 
         return process
 
@@ -293,22 +192,7 @@ class ZTFAlertStreamController(AbsProcessController):
             sub_type=AlertProcessor,
             process_name = p["name"],
         )
-        factory = AlertSource(
-            **context.loader.resolve_secrets(
-                context.loader.secrets,
-                AlertSource,
-                AlertSource.__annotations__,
-                AlertSource.__field_defaults__,
-                {'source': source},
-            )
-        )
-        processor.set_supplier(factory.get())
 
         n_alerts = processor.run()
 
-        if isinstance(factory.source, TarballSource):
-            return False
-        elif isinstance(factory.source, KafkaSource):
-            return True
-        else:
-            return n_alerts > 0
+        return True
