e78861b1d12f05239cb4a6bd92aa0d5b7ce5055c
Jakob van Santen <jvansanten@gmail.com>
2021-08-26 15:25:15 +0200

    ZTFLegacyChannelTemplate: reimplement archive T2s
    in terms of AbsT0Muxer. It is no longer possible to attach T2s to both
    alert and archival states in the same channel, but in exchange you can
    set the desired history length per channel.

--- ampel/template/ZTFLegacyChannelTemplate.py
+++ ampel/template/ZTFLegacyChannelTemplate.py
@@ -7,26 +7,21 @@
 # Last Modified Date: 30.05.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Dict, Any, ClassVar, List, Union
+from typing import Dict, Any, ClassVar, List, Optional, Union
+from ampel.model.UnitModel import UnitModel
 from pydantic import validator
 from ampel.log.AmpelLogger import AmpelLogger
 from ampel.config.builder.FirstPassConfig import FirstPassConfig
-from ampel.template.AbsEasyChannelTemplate import AbsEasyChannelTemplate, T2UnitModel
+from ampel.template.AbsEasyChannelTemplate import AbsEasyChannelTemplate
 from ampel.model.StrictModel import StrictModel
-
-
-class LegacyT2ComputeModel(StrictModel):
-	#: run these units on alerts from the stream
-	alerts: List[T2UnitModel] = []
-	#: run these units on archival light curves
-	archive: List[T2UnitModel] = []
+from ampel.model.ingest.T2Compute import T2Compute
 
 
 class ZTFLegacyChannelTemplate(AbsEasyChannelTemplate):
 	"""
 	Channel template for ZTF. Each of the named variants consumes adifferent
 	alert streams from IPAC, and produce stocks with a different set of tags:
-	
+
 	============== ============== ========================
 	Template       ZTF programids Tags
 	============== ============== ========================
@@ -39,36 +34,28 @@ class ZTFLegacyChannelTemplate(AbsEasyChannelTemplate):
 	_access: ClassVar[Dict[str, List[str]]] = {
 		"ztf_uw_private": ["ZTF", "ZTF_PUB", "ZTF_PRIV"],
 		"ztf_uw_public": ["ZTF", "ZTF_PUB"],
-		"ztf_uw_caltech": ["ZTF", "ZTF_PUB"]
+		"ztf_uw_caltech": ["ZTF", "ZTF_PUB"],
 	}
-
-	#: T2 units to trigger when transient is updated
-	t2_compute: Union[List[T2UnitModel], LegacyT2ComputeModel] = LegacyT2ComputeModel(
-		alerts=[T2UnitModel(unit="T2LightCurveSummary")]
-	) # type: ignore[assignment]
 
 	auto_complete: Any = False
 
-
-	# prevent validator from wrapping LegacyT2ComputeModel in list
-	@validator('t2_compute', pre=True, each_item=False)
-	def cast_to_list_if_required(cls, v):
-		if isinstance(v, dict) and "unit" in v:
-			return [v]
-		return v
-
+	#: include all previously ingested photopoints in emitted states
+	live_history: bool = True
+	#: include X days of archival datapoints in emitted states
+	archive_history: Optional[int] = None
 
 	# Mandatory implementation
 	def get_channel(self, logger: AmpelLogger) -> Dict[str, Any]:
 		assert self.template is not None
 		return {
 			**super().get_channel(logger),
-			'access': self.__class__._access[self.template]
+			"access": self.__class__._access[self.template],
 		}
 
-
 	# Mandatory implementation
-	def get_processes(self, logger: AmpelLogger, first_pass_config: FirstPassConfig) -> List[Dict[str, Any]]:
+	def get_processes(
+		self, logger: AmpelLogger, first_pass_config: FirstPassConfig
+	) -> List[Dict[str, Any]]:
 
 		# T3 processes
 		ret: List[Dict[str, Any]] = []
@@ -78,31 +65,48 @@ class ZTFLegacyChannelTemplate(AbsEasyChannelTemplate):
 			name = el.get("name", f"summary_{index:02d}")
 			process_name = f"{self.channel}|T3|{name}"
 			ret.append(
-				self.transfer_channel_parameters(
-					el | {"name": process_name, "tier": 3}
-				)
+				self.transfer_channel_parameters(el | {"name": process_name, "tier": 3})
 			)
 
 		if not any(model.unit == "T2LightCurveSummary" for model in self.t2_compute):
-			self.t2_compute.append(T2UnitModel(unit="T2LightCurveSummary"))
+			self.t2_compute.append(T2Compute(unit="T2LightCurveSummary"))
 
-		ret.insert(0,
+		mongo_muxer = {"unit": "ZiMongoMuxer"} if self.live_history else None
+		archive_muxer = (
+			{"unit": "ZiArchiveMuxer", "config": {"history_days": self.archive_history}}
+			if self.archive_history is not None
+			else None
+		)
+		if mongo_muxer and archive_muxer:
+			muxer: Optional[dict[str,Any]] = {
+				"unit": "ChainedT0Muxer",
+				"config": {"muxers": [mongo_muxer, archive_muxer]},
+			}
+		elif mongo_muxer:
+			muxer = mongo_muxer
+		elif archive_muxer:
+			muxer = archive_muxer
+		else:
+			muxer = None
+
+		ret.insert(
+			0,
 			self.craft_t0_process(
 				first_pass_config,
-				controller = "ZTFAlertStreamController",
-				supplier = "ZiAlertSupplier",
-				shaper = "ZiDataPointShaper",
-				muxer = "ZiMongoMuxer",
-				combiner = "ZiT1Combiner"
-			)
+				controller="ZTFAlertStreamController",
+				supplier="ZiAlertSupplier",
+				shaper="ZiDataPointShaper",
+				muxer=muxer,
+				combiner="ZiT1Combiner",
+			),
 		)
 
 		ret[0]["processor"]["config"]["loader"] = {
 			"unit": "UWAlertLoader",
 			"config": {
-				**first_pass_config['resource']['ampel-ztf/kafka'],
+				**first_pass_config["resource"]["ampel-ztf/kafka"],
 				**{"stream": self.template},
-			}
+			},
 		}
 
 		return ret
