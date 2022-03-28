d373752e31fb5decbc58f5932a1cfcee5564b1b1
wombaugh <jnordin@physik.hu-berlin.de>
2022-03-28 10:35:57 +0200

    Acknowledge alert chunks before getting the next.

--- ampel/ztf/t0/load/ZTFArchiveAlertLoader.py
+++ ampel/ztf/t0/load/ZTFArchiveAlertLoader.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File:                Ampel-ZTF/ampel/ztf/t0/load/ZTFArchiveAlertLoader.py
+# License:             BSD-3-Clause
+# Author:              jvs
+# Date:                20.10.2021
+# Last Modified Date:  28.03.2022
+# Last Modified By:    jnordin@physik.hu-berlin.de
+
+
 import logging
 from typing import Dict, Any, Union, Optional
 
@@ -5,6 +15,7 @@ import backoff
 import requests
 
 from ampel.base.AmpelBaseModel import AmpelBaseModel
+from ampel.abstract.AbsAlertLoader import AbsAlertLoader
 from ampel.model.StrictModel import StrictModel
 
 log = logging.getLogger(__name__)
@@ -19,15 +30,39 @@ class ObjectSource(StrictModel):
     archive_token: str
 
 
-class ZTFArchiveAlertLoader(AmpelBaseModel):
+class ZTFArchiveAlertLoader(AbsAlertLoader):
+    """
+
+    Load ZTF alerts from a stream provided by the
+    DESY alert archive.
+
+    The stream is initiated either by an archive stream token or
+    an archive query formulated as an ObjectSource.
+
+    get_alerts yields chunks of alerts until consumed, at which point
+    this is acknowledged and a new chunk retreieved.
+    """
+
     #: Base URL of archive service
     archive: str = "https://ampel.zeuthen.desy.de/api/ztf/archive/v2"
     #: A stream identifier, created via POST /api/ztf/archive/streams/, or a query
     stream: Union[str, ObjectSource]
 
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self._it = None
+
+
     def __iter__(self):
         return self.get_alerts()
-    
+
+    def __next__(self):
+        if not self._it:
+            self._it = iter(self)
+        return next(self._it)
+
+
+
     def get_alerts(self):
         with requests.Session() as session:
             while True:
@@ -40,16 +75,19 @@ class ZTFArchiveAlertLoader(AmpelBaseModel):
                         f"Ensure that iter_max is a multiple of the chunk size."
                     )
                     raise
+                if 'chunk' in chunk:
+                    self._acknowledge_chunk(session, chunk['chunk'])
+                    log.info(None, extra={"streamToken": self.stream, "chunk":chunk['chunk']})
                 if (
-                    isinstance(self.stream, ObjectSource)
-                    or (len(chunk["alerts"]) == 0 and chunk["chunks_remaining"] == 0)
+                        isinstance(self.stream, ObjectSource)
+                        or (len(chunk["alerts"]) == 0 and chunk["remaining"]["chunks"] == 0)
                 ):
                     break
 
     @backoff.on_exception(
         backoff.expo,
         requests.HTTPError,
-        giveup=lambda e: e.response.status_code not in {503, 504, 429, 408},
+        giveup=lambda e: e.response.status_code not in {502, 503, 504, 429, 408},
         max_time=600,
     )
     def _get_chunk(self, session: requests.Session) -> Dict[str, Any]:
@@ -67,3 +105,13 @@ class ZTFArchiveAlertLoader(AmpelBaseModel):
             response = session.get(f"{self.archive}/stream/{self.stream}/chunk")
         response.raise_for_status()
         return response.json()
+
+    @backoff.on_exception(
+        backoff.expo,
+        requests.HTTPError,
+        giveup=lambda e: e.response.status_code not in {502, 503, 504, 429, 408},
+        max_time=600,
+    )
+    def _acknowledge_chunk(self, session: requests.Session, chunk_id: int) -> None:
+        response = session.post(f"{self.archive}/stream/{self.stream}/chunk/{chunk_id}/acknowledge")
+        response.raise_for_status()
